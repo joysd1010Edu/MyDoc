12444c4a64c061ddee8f4219d6f654f0e2329e5c
Joy Sutradhar <joysutradharaj@gmail.com>
2025-02-25 03:34:51 +0600

    working ruler component

---
+++ src/app/document/[docId]/Ruler.tsx
@@ -0,0 +1,157 @@
+import { useRef, useState } from "react";
+import { FaCaretDown } from "react-icons/fa";
+
+const Marker = Array.from({ length: 83 }, (_, i) => i);
+
+const Ruler = () => {
+  const [leftMargin, setLeftMargin] = useState(56);
+  const [rightMargin, setRightMargin] = useState(56);
+  const [isDraggingLeft, setIsDraggingLeft] = useState(false);
+  const [isDraggingRight, setIsDraggingRight] = useState(false);
+  const rulerRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseDownLeft = () => {
+    setIsDraggingLeft(true);
+  };
+  const handleMouseDownRight = () => {
+    setIsDraggingRight(true);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if ((isDraggingLeft || isDraggingRight) && rulerRef.current) {
+      const Container = rulerRef.current.querySelector("#ruler-container");
+      if (Container) {
+        const ContainerRect = Container.getBoundingClientRect();
+        const relativeX = e.clientX - ContainerRect.left;
+        const rawPosition = Math.max(0, Math.min(816, relativeX));
+        if (isDraggingLeft) {
+          const MaxLeftPosition = 816 - rightMargin - 100;
+          const newLeftPosition = Math.min(rawPosition, MaxLeftPosition);
+          setLeftMargin(newLeftPosition);
+        }
+        if (isDraggingRight) {
+          const MaxRightPosition = 816 - (leftMargin + 100);
+          const newRightPosition = Math.max(816 - rawPosition, 0);
+          const constrainedRightPosition = Math.min(
+            newRightPosition,
+            MaxRightPosition
+          );
+          setRightMargin(constrainedRightPosition);
+        }
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDraggingLeft(false);
+    setIsDraggingRight(false);
+  };
+  const handleLeftDoubleClick = () => {
+    setLeftMargin(56);
+  };
+  const handleRightDoubleClick = () => {
+    setRightMargin(56);
+  };
+
+  return (
+    <div
+      ref={rulerRef}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      className="h-6  border-b border-gray-300 flex items-end relative select-none print:hidden"
+    >
+      <div
+        id="ruler-container"
+        className=" max-w-[816px]  mx-auto w-full h-full relative"
+      >
+        <MarkerCom
+          position={leftMargin}
+          isLeft={true}
+          isDragging={isDraggingLeft}
+          onMouseDown={handleMouseDownLeft}
+          onDoubleClick={handleLeftDoubleClick}
+        />
+        <MarkerCom
+          position={rightMargin}
+          isLeft={false}
+          isDragging={isDraggingRight}
+          onMouseDown={handleMouseDownRight}
+          onDoubleClick={handleRightDoubleClick}
+        />
+        <div className=" absolute inset-x-0 bottom-0 h-full">
+          <div className=" relative h-full w-[816px]">
+            {Marker.map((marker) => {
+              const position = (marker * 816) / 82;
+              return (
+                <div
+                  key={marker}
+                  className={`bottom-0  absolute  `}
+                  style={{ left: `${position}px` }}
+                >
+                  {marker % 10 === 0 && (
+                    <>
+                      <div className="absolute bottom-0 w-[1px] h-2.5 bg-neutral-500" />
+                      <span className="absolute bottom-2 text-[10px] text-neutral-500 transform -translate-x-1/2">
+                        {marker / 10 + 1}
+                      </span>
+                    </>
+                  )}
+                  {marker % 5 === 0 && marker % 10 !== 0 && (
+                    <>
+                      <div className="absolute bottom-0 w-[1px] h-1.5 bg-neutral-700" />
+                    </>
+                  )}
+                  {marker % 5 !== 0 && (
+                    <>
+                      <div className="absolute bottom-0 w-[1px] h-1 bg-neutral-700" />
+                    </>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface MarkerProps {
+  position: number;
+  isLeft: boolean;
+  isDragging: boolean;
+  onMouseDown: () => void;
+  onDoubleClick: () => void;
+}
+
+const MarkerCom = ({
+  position,
+  isLeft,
+  isDragging,
+  onMouseDown,
+  onDoubleClick,
+}: MarkerProps) => {
+  return (
+    <div
+      className="absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2"
+      style={{ [isLeft ? "left" : "right"]: `${position}px` }}
+      onMouseDown={onMouseDown}
+      onDoubleClick={onDoubleClick}
+    >
+      <FaCaretDown className=" absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2" />
+      <div
+        className=" absolute left-1/2 transform -translate-x-1/2 transition-opacity duration-150"
+        style={{
+          height: "100vh",
+          width: "1px",
+          backgroundColor: "#3B83F7",
+          transform: "scaleX(0.5)",
+          display: isDragging ? "block" : "none",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Ruler;
--- src/app/document/[docId]/editor.tsx
+++ src/app/document/[docId]/editor.tsx
@@ -19,10 +19,12 @@ import Link from "@tiptap/extension-link";
 import TextAlign from "@tiptap/extension-text-align";
 import { FontSize } from "@/extensions/Font-Size";
 import { LineHeight } from "@/extensions/lineHight";
+import Ruler from "./Ruler";
 
 const Editor = () => {
   const { setEditor } = useEditorState();
   const editor = useEditor({
+    immediatelyRender:false,
     onCreate: ({ editor }) => {
       setEditor(editor);
     },
@@ -49,9 +51,9 @@ const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: "padding-left: 56px; padding-right: 56px",
+        style: "padding-left:56px; padding-right: 56px",
         class:
-          "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] rounded-lg min-w-[820px] min-h-[1054px] flex flex-col py-10 pr-16 cursor-text",
+          "focus:outline-none print:border-0 bg-white border border-[#c7c7c7]  w-[816px] min-h-[1054px] flex flex-col py-10 pr-16 cursor-text",
       },
     },
     extensions: [
@@ -89,17 +91,30 @@ const Editor = () => {
     ],
     content: `
        
-       <h1>This is a 1st level heading</h1>
-        <h2>This is a 2nd level heading</h2>
-        <h3>This is a 3rd level heading</h3>
-        <h4>This 4th level heading will be converted to a paragraph, because levels are configured to be only 1, 2 or 3.</h4>
+       isquam voluptatibus reiciendis ut perferendis distinctio, porro nisi itaque, aliquam culpa vitae soluta. Voluptates quas eius voluptatum! Ut dicta earum rem?
+       Consequatur, ab veritatis? Perferendis cupiditate commodi accusantium rem reiciendis quibusdam vitae praesentium! Quae aut odio laudantium eum vel dolorum provident molestiae ea, cupiditate maiores. Voluptatem maiores temporibus nesciunt dignissimos? Blanditiis!
+       Labore officiis ex dolorum dolores aut recusandae tempore mollitia magnam delectus ipsam! Vero tempore eius error. Quo, sint totam natus nostrum eos veniam labore quas sed, hic iste minus corrupti.
+       Aperiam nihil suscipit quis reprehenderit provident magni quasi sint ipsam, id repudiandae ad velit nisi deleniti assumenda alias, tempore, dolore expedita libero rerum ex laborum voluptas perferendis? Ea, reiciendis magnam?
+       Molestias iusto itaque nihil recusandae sed et eos, natus laborum quidem commodi eum ratione facere sit architecto quasi cum explicabo voluptatem distinctio! Tempore earum neque molestias odit beatae iure dolores.
+       Alias odio unde facere amet veritatis quos nostrum magni asperiores reprehenderit! Impedit maiores tempore magnam vitae quia labore voluptate ea commodi amet ab expedita, quis reprehenderit, a iusto, doloremque libero?
+       Nostrum fuga esse reiciendis dolor illo deserunt id perspiciatis quidem minus nisi magnam asperiores officia amet, accusamus soluta repellendus nesciunt nihil quos fugiat quia? Optio voluptatibus eum sequi autem architecto?
+       Illum alias molestias voluptatem exercitationem facere? Itaque non iusto magnam eveniet repellendus sed, voluptatibus, nostrum enim nisi ipsam mollitia, possimus placeat rerum atque. Necessitatibus enim non, iusto numquam voluptate praesentium.
+       Nostrum, fuga quis ab quidem earum dolores vero expedita iusto facilis, quia explicabo similique culpa rem, consectetur inventore non doloremque repudiandae repellendus totam aliquid voluptatem. Itaque magnam dignissimos et adipisci.
+       Quasi quo impedit beatae, error a maiores eligendi perferendis praesentium eum repellendus pariatur placeat quidem quod maxime numquam fuga, modi aspernatur natus, deserunt amet architecto. Repudiandae deleniti saepe reprehenderit corrupti.
+       Aspernatur laudantium, suscipit quam rerum sint impedit, tenetur unde magnam, autem harum adipisci soluta similique in eos blanditiis. Aut nisi adipisci dolor quibusdam doloremque facere fuga dolorem architecto repellat ab.
+       Veritatis natus quidem quisquam fuga dicta, vitae quaerat illum consequatur tempore dolor optio, nam quia vero nostrum ea vel fugiat, eaque reprehenderit? Sapiente consequuntur, cupiditate molestias nam amet eligendi eius.
+       Possimus dolor, rerum voluptate itaque quos modi placeat, doloremque accusantium enim veritatis quibusdam, at maxime eos qui pariatur quam nesciunt! Ipsa voluptates unde veritatis non quibusdam quaerat architecto, nemo praesentium.
+       Aspernatur, voluptatibus consequuntur qui, dolorem sed nam minima fugiat repellendus quas soluta ratione! Sint, adipisci aut. Qui amet exercitationem distinctio ad esse, est, omnis eveniet necessitatibus, nesciunt doloremque totam a.
+       Sequi ratione, suscipit dolor vero minus odit quaerat corrupti quos minima atque voluptate quibusdam magnam! Consequatur quo repellat hic nostrum accusamus fuga suscipit voluptate, totam illum at cupiditate earum facere!
+       Illo recusandae corporis sint necessitatibus molestias unde assumenda reprehenderit mollitia. Unde corporis facere consequuntur veritatis et, quis minus, odit dolore error sit incidunt velit ea, a amet rem officia reiciendis.
         
       `,
   });
 
   return (
     <div className="size-full overflow-x-auto text-black bg-slate-100 px-4 print:bg-white print:overflow-visible">
-      <div className="min-w-max flex justify-center w-[820px] py-5 print:py-0 mx-auto print:w-full print:min-w-0">
+      <Ruler/>
+      <div className="min-w-max flex justify-center w-[816px] py-5 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
     </div>
